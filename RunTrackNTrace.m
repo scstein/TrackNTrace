c14754301a40a8f4abea722b8a48fd3c040271f7
Simon Christoph Stein <scstein@physik3.gwdg.de>
2015-09-22 14:55:29 +0200

    Important changes to RunTrackNTrace
    * Converted RunTrackNTrace from script to a function (which is cleaner)
    * generalOptions.lastFrame is now set to the correct number. Before it
    was left to be 'inf' if the user did not specify a specific frame.
    * Now also movieSize=size(movie) is saved to the TNT file

--- RunTrackNTrace.m
+++ RunTrackNTrace.m
@@ -1,147 +1,152 @@
-%% Add required folders and subfolders to path
-addpath(genpath('external'));
-addpath(genpath('helper'));
-addpath(genpath('subfun'));
-addpath(genpath('testing'));
+function RunTrackNTrace()
+    %% Add required folders and subfolders to path
+    fullPathToThisFile = mfilename('fullpath');
+    [path,~,~] = fileparts(fullPathToThisFile);
+    addpath(genpath([path,filesep,'external']));
+    addpath(genpath([path,filesep,'helper']));
+    addpath(genpath([path,filesep,'subfun']));
+    addpath(genpath([path,filesep,'analysis']));
 
-%% Load and adjust the default settings for this batch
-GUIinputs.titleText = 'Please select a list of movies to process.';
-GUIinputs.fileText  = 'Default settings for this batch';
-GUIinputs.singleFileMode = false;
-[generalOptions_def, candidateOptions_def,fittingOptions_def,trackingOptions_def] = setDefaultOptions();
-[generalOptions_def, candidateOptions_def,fittingOptions_def,trackingOptions_def, GUIreturns_def] = settingsGUI(generalOptions_def, candidateOptions_def,fittingOptions_def,trackingOptions_def, GUIinputs);
-if GUIreturns_def.userExit; error('User abort. Stopping TrackNTrace.'); end;
+    %% Load and adjust the default settings for this batch
+    GUIinputs.titleText = 'Please select a list of movies to process.';
+    GUIinputs.fileText  = 'Default settings for this batch';
+    GUIinputs.singleFileMode = false;
+    [generalOptions_def, candidateOptions_def,fittingOptions_def,trackingOptions_def] = setDefaultOptions();
+    [generalOptions_def, candidateOptions_def,fittingOptions_def,trackingOptions_def, GUIreturns_def] = settingsGUI(generalOptions_def, candidateOptions_def,fittingOptions_def,trackingOptions_def, GUIinputs);
+    if GUIreturns_def.userExit; error('User abort. Stopping TrackNTrace.'); end;
 
-%% Adjust options for each movie and test settings if desired
-GUIinputs.singleFileMode = true; % No editing of movie list possible
+    %% Adjust options for each movie and test settings if desired
+    GUIinputs.singleFileMode = true; % No editing of movie list possible
 
-% [movie_list,dark_stack] = getMovieFilenames(generalOptions.filename_movies, generalOptions.filename_dark_movie);
-movie_list = generalOptions_def.filename_movies;
-% Calculate default dark image if given
-dark_img_def = [];
-if(~isempty(generalOptions_def.filename_dark_movie))
-    dark_img_def = CalculateDark(read_tiff(generalOptions_def.filename_dark_movie));
-end
-
-% Get timestamp for output files
-time = clock;
-timestamp = sprintf('%i-m%02i-d%02i-%ih%i',time(1),time(2),time(3),time(4),time(5));
-
-posFit_list = cell(0);
-for i=1:numel(movie_list)
-    filename_movie = movie_list{i};
-    [path,filename,~] = fileparts(filename_movie);
-    filename_fitData = [path,filesep,filename,'_',timestamp,'_TNT.mat'];
-    
-    
-    %     Skip nonsensical input
-    [T,movie] = evalc(['read_tiff(''',filename_movie,''',false,[1,2])']); % Read 2 frames. note: evalc suppresses output
-    if size(movie,3)<=1
-       continue; 
+    % [movie_list,dark_stack] = getMovieFilenames(generalOptions.filename_movies, generalOptions.filename_dark_movie);
+    movie_list = generalOptions_def.filename_movies;
+    % Calculate default dark image if given
+    dark_img_def = [];
+    if(~isempty(generalOptions_def.filename_dark_movie))
+        dark_img_def = CalculateDark(read_tiff(generalOptions_def.filename_dark_movie));
     end
-    
-    % Set options to default for this batch
-    generalOptions = generalOptions_def;
-    candidateOptions = candidateOptions_def;
-    fittingOptions = fittingOptions_def;
-    trackingOptions = trackingOptions_def;
-    dark_img = dark_img_def;
-    
-    % Does the user want to adjust the settings per movie?
-    if not(GUIreturns_def.useSettingsForAll)
-        % Show filename in GUI
-        GUIinputs.fileText = filename_movie;
-        
-        GUIinputs.titleText = 'Adjust movie specific options.';
-        [generalOptions, candidateOptions,fittingOptions,trackingOptions, GUIreturns] = settingsGUI(generalOptions, candidateOptions,fittingOptions,trackingOptions, GUIinputs);
-        if GUIreturns.userExit; error(sprintf('User abort. Stopping TrackNTrace.\nDelete unwanted settings files that might have been saved already.')); end;
-        
-        % Check if different dark movie was given
-        if(~strcmp(generalOptions_def.filename_dark_movie, generalOptions.filename_dark_movie))
-            if(~isempty(generalOptions.filename_dark_movie))
-                dark_img   = CalculateDark(read_tiff(generalOptions.filename_dark_movie));
-            end
+
+    % Get timestamp for output files
+    time = clock;
+    timestamp = sprintf('%i-m%02i-d%02i-%ih%i',time(1),time(2),time(3),time(4),time(5));
+
+    posFit_list = cell(0);
+    for i=1:numel(movie_list)
+        filename_movie = movie_list{i};
+        [path,filename,~] = fileparts(filename_movie);
+        filename_fitData = [path,filesep,filename,'_',timestamp,'_TNT.mat'];
+
+
+        %     Skip nonsensical input
+        [~,movie] = evalc(['read_tiff(''',filename_movie,''',false,[1,2])']); % Read 2 frames. note: evalc suppresses output
+        if size(movie,3)<=1
+            continue;
         end
-        
-        % If test mode is enabled, analyze first X frames and show GUI
-        if generalOptions.previewMode
-            run_again = true;
-            first_run = true;
-            filename_dark_movie = generalOptions.filename_dark_movie;
-            while run_again
-                if not(first_run);
-                    [generalOptions, candidateOptions,fittingOptions,trackingOptions, GUIreturns] = settingsGUI(generalOptions, candidateOptions,fittingOptions,trackingOptions, GUIinputs);
-                    if GUIreturns.userExit; error(sprintf('User abort. Stopping TrackNTrace.\nDelete unwanted settings files that might have been saved already.')); end;
-                end;
-            
-                if not(generalOptions.previewMode); break; end; % If test mode was disabled by user in the settingsGUI
-                % Check if requested frame interval has changed -> re-read movie if neccessary
-                if first_run || GUIreturns.testWindowChanged
-                    movie = read_tiff(filename_movie, false, [generalOptions.firstFrameTesting, generalOptions.lastFrameTesting]);
+
+        % Set options to default for this batch
+        generalOptions = generalOptions_def;
+        candidateOptions = candidateOptions_def;
+        fittingOptions = fittingOptions_def;
+        trackingOptions = trackingOptions_def;
+        dark_img = dark_img_def;
+
+        % Does the user want to adjust the settings per movie?
+        if not(GUIreturns_def.useSettingsForAll)
+            % Show filename in GUI
+            GUIinputs.fileText = filename_movie;
+
+            GUIinputs.titleText = 'Adjust movie specific options.';
+            [generalOptions, candidateOptions,fittingOptions,trackingOptions, GUIreturns] = settingsGUI(generalOptions, candidateOptions,fittingOptions,trackingOptions, GUIinputs);
+            if GUIreturns.userExit; error(sprintf('User abort. Stopping TrackNTrace.\nDelete unwanted settings files that might have been saved already.')); end; %#ok<SPERR>
+
+            % Check if different dark movie was given
+            if(~strcmp(generalOptions_def.filename_dark_movie, generalOptions.filename_dark_movie))
+                if(~isempty(generalOptions.filename_dark_movie))
+                    dark_img   = CalculateDark(read_tiff(generalOptions.filename_dark_movie));
                 end
-                % Check if different dark movie was given
-                if(~strcmp(filename_dark_movie, generalOptions.filename_dark_movie))
-                    if(~isempty(generalOptions.filename_dark_movie))
-                        dark_img   = CalculateDark(read_tiff(generalOptions.filename_dark_movie));
+            end
+
+            % If test mode is enabled, analyze first X frames and show GUI
+            if generalOptions.previewMode
+                run_again = true;
+                first_run = true;
+                filename_dark_movie = generalOptions.filename_dark_movie;
+                while run_again
+                    if not(first_run);
+                        [generalOptions, candidateOptions,fittingOptions,trackingOptions, GUIreturns] = settingsGUI(generalOptions, candidateOptions,fittingOptions,trackingOptions, GUIinputs);
+                        if GUIreturns.userExit; error(sprintf('User abort. Stopping TrackNTrace.\nDelete unwanted settings files that might have been saved already.')); end; %#ok<SPERR>
+                    end;
+
+                    if not(generalOptions.previewMode); break; end; % If test mode was disabled by user in the settingsGUI
+                    % Check if requested frame interval has changed -> re-read movie if neccessary
+                    if first_run || GUIreturns.testWindowChanged
+                        movie = read_tiff(filename_movie, false, [generalOptions.firstFrameTesting, generalOptions.lastFrameTesting]);
                     end
-                    filename_dark_movie = generalOptions.filename_dark_movie;
+                    % Check if different dark movie was given
+                    if(~strcmp(filename_dark_movie, generalOptions.filename_dark_movie))
+                        if(~isempty(generalOptions.filename_dark_movie))
+                            dark_img   = CalculateDark(read_tiff(generalOptions.filename_dark_movie));
+                        end
+                        filename_dark_movie = generalOptions.filename_dark_movie;
+                    end
+                    % IF: this is the first run, the preview window changed or the fitting/candidate options changed locate and
+                    % track particles and save fitData. ELSE: reuse fitData acquired in the last run without re-fitting
+                    if  first_run || GUIreturns.testWindowChanged || GUIreturns.fittingOptionsChanged || GUIreturns.candidateOptionsChanged || GUIreturns.fittingOptionsChanged
+                        [run_again, fitData_test] = testTrackerSettings(movie,dark_img,candidateOptions,fittingOptions,trackingOptions);
+                    else
+                        [run_again] = testTrackerSettings(movie,dark_img,candidateOptions,fittingOptions,trackingOptions, fitData_test);
+                    end
+                    first_run = false;
                 end
-                % IF: this is the first run, the preview window changed or the fitting/candidate options changed locate and
-                % track particles and save fitData. ELSE: reuse fitData acquired in the last run without re-fitting
-                if  first_run || GUIreturns.testWindowChanged || GUIreturns.fittingOptionsChanged || GUIreturns.candidateOptionsChanged || GUIreturns.fittingOptionsChanged
-                    [run_again, fitData_test] = testTrackerSettings(movie,dark_img,candidateOptions,fittingOptions,trackingOptions);
-                else
-                    [run_again] = testTrackerSettings(movie,dark_img,candidateOptions,fittingOptions,trackingOptions, fitData_test);
-                end
-                first_run = false;
             end
+
         end
-        
-    end
-    % Save options
-    generalOptions.filename_movies = {filename_movie}; % Save only name of this file in its settings (important when loading options)
-    save(filename_fitData,'filename_movie','generalOptions','candidateOptions','fittingOptions','trackingOptions','dark_img');
-    posFit_list = [posFit_list;{filename_fitData}]; %#ok<AGROW>
-end
-
-%% Compute positions
-clearvars -except posFit_list
-for i=1:numel(posFit_list)
-    filename_fitData = posFit_list{i};
-    load(filename_fitData,'-mat');
-    
-    % Read movie  
-    movie = read_tiff(filename_movie, false, [generalOptions.firstFrame,generalOptions.lastFrame]);
-    
-    % Compute the positions
-    fprintf('######\nLocating particles in movie %s.\n',filename_movie);
-    fitData = locateParticles(movie, dark_img, candidateOptions, fittingOptions); %#ok<NASGU>
-    
-    % Save positions
-    save(filename_fitData,'fitData','-append');
-end
-
-clear fitData movie
-%% TODO
-% correct for half pixel shift?
-% keep amplitude even if tracker cant manage
-% replace Ptest by something better? --> amp>mean+k*std, k by user
-% automatically determine if memory large enough
-
-%% Compute trajectories
-for i=1:numel(posFit_list)
-    load(posFit_list{i},'trackingOptions','fitData','filename_movie');
-    
-    % If no tracking is desired for this movie, continue
-    if (~trackingOptions.enableTracking)
-        continue
+        % Save options
+        generalOptions.filename_movies = {filename_movie}; % Save only name of this file in its settings (important when loading options)
+        save(filename_fitData,'filename_movie','generalOptions','candidateOptions','fittingOptions','trackingOptions','dark_img');
+        posFit_list = [posFit_list;{filename_fitData}]; %#ok<AGROW>
     end
 
-    % Compute trajectories
-    fprintf('######\nTracking particles in movie %s.\n',filename_movie);
-    trajectoryData = trackParticles(fitData,trackingOptions);
-    
-    %Save trajectories
-    save(posFit_list{i},'trajectoryData','-append');
+    %% Compute positions
+    clearvars -except posFit_list
+    for i=1:numel(posFit_list)
+        filename_fitData = posFit_list{i};
+        load(filename_fitData,'-mat');
+
+        % Read movie
+        movie = read_tiff(filename_movie, false, [generalOptions.firstFrame,generalOptions.lastFrame]);        
+        % Compute the positions
+        fprintf('######\nLocating particles in movie %s.\n',filename_movie);
+        fitData = locateParticles(movie, dark_img, candidateOptions, fittingOptions);
+
+        % Save positions and movieSize, update generalOptions.lastFrame
+        generalOptions.lastFrame = generalOptions.firstFrame + size(movie,3)-1; % lastFrame could have been set to 'inf', now we synchronize with the correct number
+        movieSize = size(movie); %#ok<NASGU> % Save size of movie (nice to have)
+        save(filename_fitData,'fitData','generalOptions','movieSize','-append');
+    end
+
+    clear fitData movie
+    %% TODO
+    % correct for half pixel shift?
+    % keep amplitude even if tracker cant manage
+    % replace Ptest by something better? --> amp>mean+k*std, k by user
+    % automatically determine if memory large enough
+
+    %% Compute trajectories
+    for i=1:numel(posFit_list)
+        load(posFit_list{i},'trackingOptions','fitData','filename_movie');
+
+        % If no tracking is desired for this movie, continue
+        if (~trackingOptions.enableTracking)
+            continue
+        end
+
+        % Compute trajectories
+        fprintf('######\nTracking particles in movie %s.\n',filename_movie);
+        trajectoryData = trackParticles(fitData,trackingOptions); %#ok<NASGU>
+
+        %Save trajectories
+        save(posFit_list{i},'trajectoryData','-append');
+    end
 end
 
