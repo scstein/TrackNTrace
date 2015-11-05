c0eed7150285456655f598559c9fe5a5b068c1b1
Simon Christoph Stein <scstein@physik3.gwdg.de>
2015-11-05 13:23:51 +0100

    Implemented callbacks with parameter saving
    Saving & loading is done by placing the data with setappdata into the
    figure.

--- subfun/0_test_OptionsGUI.m
+++ subfun/0_test_OptionsGUI.m
@@ -1,19 +1,22 @@
 % Name of the component these options are for
-name = 'Cross correlation';
+plugin_name = 'Cross correlation';
 
 % Enter names of the parameters
-par_text  = {'param a','bool param','NiceParam List','name'};
+% These translate to the names of variables inside options struct this plugin
+% outputs by removing all white spaces.
+par_name  = {'Param a','bool param','NiceParam List','Name'};
 
 % Enter type of the parameters
 % possible: 'float', 'int', 'bool','list'
-par_type  = {'float','bool','list','float'};
+par_type  = {'int','bool','list','float'};
 
 % Default value for parameters
 % Should be a number for 'float'/'int', true/false for 'bool'
 % or a cell array string list of possible choices for 'list' (first entry is default)
-par_value = {1.263,true,{'Option A','Option B','Option Jan'},3.2};
+par_defaultValue = {1.263,true,{'Option A','Option B','Option C'},3.2};
 
 % Tooltip for the parameters
 par_tooltip = {'This is a very long sentence for a tooltip.', 'does', 'help','blub'};
 
-h = createOptionsGUI(name, par_text, par_type, par_value, par_tooltip);
+h = createOptionsGUI(plugin_name, par_name, par_type, par_defaultValue, par_tooltip); %, inputOptions);
+%options = getappdata(h,'options')
--- subfun/createOptionsGUI.m
+++ subfun/createOptionsGUI.m
@@ -1,10 +1,32 @@
-function h = createOptionsGUI( name, par_text, par_type, par_value, par_tooltip)
+function h = createOptionsGUI( plugin_name, par_name, par_type, par_defaultValue, par_tooltip, inputOptions)
 %CREATEOPTIONSGUI Summary of this function goes here
 %   Detailed explanation goes here
 
+% All paramters are saved within the options struct. Variable names are
+% derived from the given parameter name by erasing all white spaces.
+if nargin < 6 || isempty(inputOptions)
+    options = [];
+    options.plugin_name = plugin_name;
+else
+    if(plugin_name == inputOptions.plugin_name)
+        options = inputOptions;
+    else
+        error('createOptionsGUI: Tried to create options for plugin ''%s'' with inputOptions for ''%s''.');
+    end
+end
+
+% strip white spaces from paramter names
+struct_varNames = cell(numel(par_name));
+for iP = 1:numel(par_name);
+   stripped_name =  par_name{iP};
+   stripped_name(stripped_name == ' ') = [];
+   struct_varNames{iP} = stripped_name;
+end
+
+
 h = figure('NumberTitle','off');
 
-num_pars = numel(par_text);
+num_pars = numel(par_name);
 % set(h,'Name',[name, ' Options']);
 set(h,'Units','points');
 set(h,'Resize','off');
@@ -23,10 +45,10 @@ editSize = 6*fontSize; % Size of edit fields
 
 
 % Title text
-text = [name, ' Options'];
+plugin_name = [plugin_name, ' Options'];
 text_pos = [col_spacing, win_heigth-2*fontSize, 1,elemHeigth];
 h_text = uicontrol('Parent',h, 'Units','points', 'Style','text',...
-    'BackgroundColor',bg_color,'FontSize',fontSize,'FontWeight','bold','String',text);
+    'BackgroundColor',bg_color,'FontSize',fontSize,'FontWeight','bold','String',plugin_name);
 % Resize text to full width
 extent = get(h_text,'Extent');
 text_pos(3) = extent(3);
@@ -34,15 +56,23 @@ set(h_text,'Position',text_pos);
 
 max_width = text_pos(3)+2*col_spacing;
 
+% Create controls for all parameters
 for iP = 1:num_pars
-    text = par_text{iP};
-    ptype = par_type{iP};
-    value = par_value{iP};
+    pName = par_name{iP};
+    pStructVarName = struct_varNames{iP};
+    pType = par_type{iP};
+    
+    % Check if value for parameter was given
+    if(isfield(options,pStructVarName))
+        pValue = options.(pStructVarName);
+    else
+        pValue = par_defaultValue{iP};
+    end
     
     % Add text
     text_pos = [col_spacing, win_heigth-5*fontSize-(iP-1)*row_spacing, 1, elemHeigth];
     h_text = uicontrol('Parent',h, 'Units','points','Style','text', ...
-        'BackgroundColor',bg_color,'FontSize',fontSize,'String',text);
+        'BackgroundColor',bg_color,'FontSize',fontSize,'String',pName);
     extent = get(h_text,'Extent');
     text_pos(3) = extent(3);
     set(h_text,'Position',text_pos);
@@ -53,33 +83,52 @@ for iP = 1:num_pars
     
     % Add uicontrol for value
     value_position = [col_spacing+textwidth+col_spacing, win_heigth-5*fontSize-(iP-1)*row_spacing, 1, 1];
-    switch ptype
+    switch pType
         case 'float'
             val_pos = [value_position(1), value_position(2), editSize, elemHeigth];
             h_val=uicontrol('Parent',h, 'Units','points', 'Position', val_pos, ...
-                'Style','edit','FontSize',fontSize,'String',num2str(value));% ,'Callback', @changeText);
+                'Style','edit','FontSize',fontSize,'String',num2str(pValue));
         case 'int'
             val_pos = [value_position(1), value_position(2), editSize, elemHeigth];
             h_val= uicontrol('Parent',h, 'Units','points', 'Position', val_pos, ...
-                'Style','edit','FontSize',fontSize,'String',num2str(value));% ,'Callback', @changeText);
+                'Style','edit','FontSize',fontSize,'String',num2str(pValue));
         case 'bool'
-            val_pos = [value_position(1), value_position(2), fontSize, elemHeigth];
+            val_pos = [value_position(1), value_position(2), 11.5, elemHeigth];
             h_val= uicontrol('Parent',h, 'Units','points', 'Position', val_pos, ...
-                'Style','checkbox','FontSize',fontSize,'Value',value);% ,'Callback', @changeText);
+                'Style','checkbox','FontSize',fontSize,'Value',pValue,'String','');
         case 'list'
-            % TODO: Determine length of largest string, set extent based on this
             val_pos = [value_position(1), value_position(2), editSize, elemHeigth];
             h_val = uicontrol('Parent',h, 'Units','points', 'Position', val_pos, ...
-                'Style','popupmenu','FontSize',fontSize,'String',value);% ,'Callback', @changeText);
-            extent = get(h_val,'Extent');
-            val_pos(3) = extent(3)+16;
+                'Style','popupmenu','FontSize',fontSize,'String',par_defaultValue{iP});
+            % Determine neccessary width of popup based on choosable options
+            maxCharacters = 0;
+            for iOption = 1:numel(par_defaultValue{iP})
+                characters = numel(par_defaultValue{iP}{iOption});
+                if(characters>maxCharacters); maxCharacters = characters; end;                    
+            end
+            set(h_val,'Value', 1);
+            val_pos(3) = maxCharacters*fontSize/2+16; % Note: fontSize/2 is an (arbitrary) approximation of the font width
             set(h_val,'Position',val_pos);
+            
+            % Special case for lists: The given default value is the list 
+            % options. If no selected option was given, the first item will
+            % be automatically selected. If a selected option was given, 
+            % we have to set the popup accordingly.
+            if(isfield(options,pStructVarName))
+                setPopup(h_val, pValue);
+            end
+            
         otherwise
-            error('Unkown parameter type');
+            error('createOptionsGUI: Unknown parameter type');
     end
     
     % Set Tooltip
     set(h_val, 'TooltipString',par_tooltip{iP});
+    
+    % Set callback
+    set(h_val, 'Callback', {@callback_saveOnChange,pStructVarName,pType});
+    % Execute callback once to save the parameter starting value
+    callback_saveOnChange(h_val, [], pStructVarName, pType);
     
     overall_width = 3*col_spacing + text_pos(3) + val_pos(3);
     if(overall_width> max_width)
@@ -104,5 +153,40 @@ movegui(h,'center');
 %     'ButtonDownFcn',@printPosition,'NextPlot','replacechildren');
 
 
+    function callback_saveOnChange(uiObj, eventdata, structVarName, pType)       
+        switch pType
+            case 'float'
+                pValue = str2double(get(uiObj,'String'));
+                set(uiObj, 'String', num2str(pValue)); % Synchronise text field
+                options.(structVarName) = pValue;
+            case 'int'
+                pValue = round(str2double(get(uiObj,'String'))); % get integer value
+                set(uiObj, 'String', num2str(pValue)); % Synchronise text field
+                options.(structVarName) = pValue;
+            case 'bool'
+                pValue = logical(get(uiObj,'Value'));
+                options.(structVarName) = pValue;
+            case 'list'
+                choices = get(uiObj,'String');
+                optionString = choices{get(uiObj,'Value')};
+                options.(structVarName) = optionString;
+            otherwise
+                error('createOptionsGUI:callback_saveOnChange: Unknown parameter type');                              
+        end
+        setappdata(h,'options',options); % Update stored options
+    end
+
+
+% Sets a popup menu to the option with name 'optionString'
+    function setPopup(hObj,optionString)
+        choices = get(hObj,'String');
+        for idx = 1:length(choices)
+            if strcmp(choices{idx}, optionString)
+                set(hObj,'Value',idx);
+                return
+            end
+        end
+        error('invalid option ''%s'' to setPopup tag: %s',optionString,get(hObj,'Tag'));
+    end
 end
 
